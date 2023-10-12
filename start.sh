37c3e3eaa64f5f8131bc3db2e716cca0b0044f6f
sirdzzz <chebotarev.da@phystech.edu>
2023-10-12 19:33:23 +0300

    start.sh throws error if number of points not specified

--- start.sh
+++ start.sh
@@ -1,6 +1,29 @@
 #!/bin/bash
 
-readonly total_points=100000
+total_points=100000
+draw=0
+while [[ "$#" -gt 0 ]]; do
+    case "$1" in
+        -d|--draw)
+            draw=1
+            shift
+            ;;
+        -p|--points)
+			if [[ -n "$1" ]]; then
+                echo "Error: number of points not specified!"
+                exit 1
+            fi
+			total_points=$2
+			shift
+			;;
+        *) 
+            echo "Invalid argument: $1"
+            exit 1
+            ;;
+    esac
+    shift
+done
+
 
 gcc -fopenmp -o pi_calc_lin.out pi_calc_lin.c
 ./pi_calc_lin.out $total_points
@@ -8,4 +31,6 @@ gcc -fopenmp -o pi_calc_lin.out pi_calc_lin.c
 gcc -fopenmp -o pi_calc.out pi_calc.c
 ./pi_calc.out $total_points
 
-python3 draw.py
+if [ $draw -eq 1 ]; then
+    python3 draw.py
+fi
